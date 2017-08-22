6bc50662fa50f93db25d71fc413f538f237648c3
Mandorlo <shaqed@outlook.fr>
2017-08-22 16:43:59 +0200

    details_tr page is now editable

--- src/pages/tr-details/tr-details.ts
+++ src/pages/tr-details/tr-details.ts
@@ -8,8 +8,9 @@ import { DatePicker } from '@ionic-native/date-picker';
 
 import moment from 'moment';
 import * as pik from 'pikaday';
+import _ from 'lodash';
 
-// TODO : delete tr
+
 
 
 @Component({
@@ -18,14 +19,31 @@ import * as pik from 'pikaday';
 })
 export class TrDetailsPage {
   @ViewChild('myPicker') mypicker;
-  private pickerObject:any;
+  private pickerObject: any;
 
   private processing: boolean = false;
   private orig_tr: Object;
   private curr_tr: Object;
   private icon: string = "";
-  private traduction_type = { 'in': 'Revenu', 'out': 'Dépense', 'retrait': 'Retrait', 'depot': "Dépôt" };
+  private traduction_type = {
+    'in': {
+      'banque': 'Revenu sur mon compte bancaire',
+      'caisse': 'Revenu en cash'
+    }, 'out': {
+      'banque': 'Dépense par carte ou chèque',
+      'caisse': 'Dépense en liquide'
+    }, 'retrait': {
+      'banque': 'Retrait', 'caisse': 'Retrait'
+    }, 'depot': {
+      'banque': "Dépôt", 'caisse': "Dépôt"
+    }
+  };
   private pretty: Object = { 'montant': "0 €", "date": "", "date2": "", "category": "", 'type': '' };
+  private categories_options = [];
+  private show_choixCategory: boolean = false;
+  private moyen_options = [];
+  private show_choixMoyen: boolean = false;
+
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -41,12 +59,28 @@ export class TrDetailsPage {
     this.pretty['date'] = moment(this.curr_tr["date"]).fromNow();
     this.pretty['date2'] = moment(this.curr_tr['date']).format("ddd D MMM YYYY");
     this.pretty['category'] = this.trService.categoryLabel(this.curr_tr);
-    this.pretty['type'] = this.traduction_type[this.curr_tr['type']];
+    this.pretty['type'] = this.traduction_type[this.curr_tr['type']][this.curr_tr['moyen']];
+
+    this.categories_options = this.getCatOptions(this.curr_tr['type']);
+
+    this.moyen_options = [{ 'val': 'banque', 'title': 'J\'ai payé par carte ou chèque', 'icon': 'fa-credit-card' },
+    { 'val': 'caisse', 'title': 'J\'ai payé en argent liquide', 'icon': 'fa-money' }];
+    if (this.curr_tr['type'] == "in") {
+      this.moyen_options = [{ 'val': 'banque', 'title': 'J\'ai reçu de l\'argent en banque', 'icon': 'fa-credit-card' },
+      { 'val': 'caisse', 'title': 'J\'ai reçu de l\'argent liquide', 'icon': 'fa-money' }]
+    }
   }
 
   ionViewDidLoad() {
     console.log('TrDetailsPage : Signore, tu solo hai parole di vita eterna');
     this.setupDatePicker();
+  }
+
+  getCatOptions(type) {
+    let curr_catlist = (type == "out") ? this.paramService.categories : this.paramService.categories_in;
+    return _.map(curr_catlist, el => {
+      return { 'val': el.id, 'title': el.label, 'icon': el.icon }
+    });
   }
 
   presentToast(msg) {
@@ -129,13 +163,13 @@ export class TrDetailsPage {
       },
       err => {
         console.log('Error occurred while getting date: ', err);
-        console.log("Trying with ionic datepicker...TODO");
+        console.log("Mayb that's because 'm in a browsers, I'll try with the pikaday datepicker...");
         this.pickerObject.show();
       });
   }
 
   setupDatePicker() {
-    let field:HTMLDivElement = this.mypicker.nativeElement;
+    let field: HTMLDivElement = this.mypicker.nativeElement;
     console.log("pik", field, this.mypicker);
     let mythis = this;
     this.pickerObject = new pik({
@@ -149,6 +183,63 @@ export class TrDetailsPage {
       },
       format: 'D MMM YYYY'
     });
+  }
+
+  showCategoryPrompt() {
+    if (this.curr_tr['type'] == 'in' || this.curr_tr['type'] == 'out')
+      this.show_choixCategory = true;
+  }
+
+  changeCategory() {
+    this.pretty['category'] = this.trService.categoryLabel(this.curr_tr);
+    this.icon = this.trService.smartIcon(this.curr_tr);
+    this.show_choixCategory = false;
+  }
+
+  showMoyenPrompt() {
+    if (this.curr_tr['type'] == 'in' || this.curr_tr['type'] == 'out')
+      this.show_choixMoyen = true;
+  }
+
+  changeMoyen() {
+    this.categories_options = this.getCatOptions(this.curr_tr['type']);
+    this.pretty['type'] = this.traduction_type[this.curr_tr['type']][this.curr_tr['moyen']];
+    this.show_choixMoyen = false;
+  }
+
+  changeMontant() {
+    let alert = this.alertCtrl.create({
+      title: 'Modifier le montant',
+      inputs: [
+        {
+          name: 'montant',
+          placeholder: 'xxx €',
+          value: this.curr_tr['montant'].toString()
+        }
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Modifier',
+          handler: data => {
+            let m = parseFloat(data.montant);
+            if (!isNaN(m)) {
+              this.curr_tr['montant'] = m;
+              this.pretty['montant'] = this.curr_tr['montant'].toString() + " " + this.paramService.symbolCurrency();
+            } else {
+              this.presentToast("Le montant inséré n'est pas valide !")
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
